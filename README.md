1984ed396cefd947b23d60df40ae1a5a0bca82c6
RolandHyden <Roland.Hyden@RegionHalland.se>
2019-02-22 13:56:01 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# region-halland-acf-page-ingress
-Skapa en textruta med plats för ingress högst upp på en sida
+# Skapa en textruta med plats för ingress högst upp på en sida
+
+## Hur man använder Region Hallands plugin "region-halland-acf-page-ingress"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-page-ingress".
+
+
+## Användningsområde
+
+Denna plugin skapar en textruta med plats för ingress högst upp på en sida
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-page-ingress.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-page-ingress.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-page-ingress": "1.0.0"
+},
+```
+
+
+## Visa ingressen på en sida via "Blade"
+
+```sh
+<div>
+  {{ get_region_halland_acf_page_ingress() }}
+</div>
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_page_ingress.php
@@ -0,0 +1,99 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Page Ingress
+	 */
+	/*
+	Plugin Name: Region Halland ACF Page Ingress
+	Description: ACF-fält för ingress högst upp på en sida
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa function om ACF är installerad
+	add_action('acf/init', 'my_acf_add_page_ingress_field_groups');
+
+	// Function för att lägga till "field groups"
+	function my_acf_add_page_ingress_field_groups() {
+
+		if (function_exists('acf_add_local_field_group')):
+
+			acf_add_local_field_group(array(
+			    'key' => 'group_1000107',
+			    'title' => ' ',
+			    'fields' => array(
+			        0 => array(
+			        	'key' => 'field_1000108',
+			            'label' => __('Ingress', 'regionhalland'),
+			            'name' => 'name_1000109',
+			            'type' => 'textarea',
+			            'instructions' => __('Skriv din ingress här', 'regionhalland'),
+			            'required' => 0,
+			            'conditional_logic' => 0,
+			            'wrapper' => array(
+			                'width' => '',
+			                'class' => '',
+			                'id' => '',
+			            ),
+			            'default_value' => '',
+			            'placeholder' => '',
+			            'prepend' => '',
+			            'append' => '',
+			            'maxlength' => '',
+			        ),
+			    ),
+			    'location' => array(
+			        0 => array(
+			            0 => array(
+			                'param' => 'post_type',
+			                'operator' => '==',
+			                'value' => 'page',
+			            ),
+			        )
+			    ),
+			    'menu_order' => 0,
+			    'position' => 'normal',
+			    'style' => 'default',
+			    'label_placement' => 'top',
+			    'instruction_placement' => 'label',
+			    'hide_on_screen' => '',
+			    'active' => 1,
+			    'description' => '',
+			));
+
+		endif;
+	
+	}
+
+	// Anropa metod för att lägga till js-fil
+	add_action('admin_enqueue_scripts', 'my_enqueue_for_ingress');
+
+	// Metod för att lägga till js-fil
+	function my_enqueue_for_ingress($hook) {
+		wp_enqueue_script('my_custom_script', plugin_dir_url(__FILE__) . '/js/ingress.js');
+	}
+
+	// Returnera ingress
+	function get_region_halland_acf_page_ingress() {
+		return get_field('name_1000109');	
+	}
+	
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_page_ingress_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_acf_page_ingress_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_acf_page_ingress_activate');
+
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_acf_page_ingress_deactivate');
+
+?>
